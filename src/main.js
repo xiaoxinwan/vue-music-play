53ce4f683f4333c1c07ff3a7901749ab67ac8486
xiaoxinwan <lixinxi1010@gmail.com>
2019-03-17 13:35:57 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+import fastclick from 'fastclick'
+import './assets/stylus/index.styl'
 
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  // router,
   render: h => h(App)
 }).$mount('#app')
