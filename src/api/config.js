484999bfbf2592071b1fe185c175f1e889332cf4
xiaoxinwan <lixinxi1010@gmail.com>
2019-03-17 18:40:22 +0800

    添加jsonp.js 和 获取recommend的接口

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export const commonParams = {
+    g_tk: 59384408,
+    inCharset: 'utf-8',
+    outCharset: 'utf-8',
+    notice: 0,
+    format: 'json'
+}
+
+export const options  = {
+    param: ''
+}
+
+export const ERR_OK = 0
