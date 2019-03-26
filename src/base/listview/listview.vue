11c23ed4228a5a50b9ab945f4b88d302df880b01
xiaoxinwan <lixinxi1010@gmail.com>
2019-03-26 21:03:16 +0800

    歌手页面的快速入口支持滑动至相应项

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -2,9 +2,14 @@
     <scroll class="listview"
             :data="data"
             :probe-type="probeType"
+            ref="listview"
     >
         <ul>
-            <li v-for="group in data" class="list-group" :key="group.title">
+            <li v-for="group in data"
+                class="list-group"
+                :key="group.title"
+                ref="listGroup"
+            >
                 <h2 class="list-group-title">{{group.title}}</h2>
                 <ul>
                     <li v-for="item in group.items" :key="item.id" class="list-group-item">
@@ -14,12 +19,28 @@
                 </ul>
             </li>
         </ul>
+        <div class="list-shortcut"
+             @touchstart="onShortcutTouchStart"
+             @touchmove.stop.prevent="onShortcutTouchMove"
+             @touchend.stop
+        >
+            <ul>
+                <li v-for="(item,index) in shortcutList"
+                    :data-index="index"
+                    :key="item"
+                    class="item">
+                    {{item}}
+                </li>
+            </ul>
+        </div>
     </scroll>
 </template>
 
 <script>
     import Scroll from '../scroll/scroll'
+    import {getData} from "../../assets/js/dom";
 
+    const ANCHOR_HEIGHT = 18
     export default {
         name: "listView",
         components: {
@@ -31,8 +52,35 @@
                 default: () => []
             }
         },
+        computed: {
+            shortcutList() {
+                return this.data.map((group) => {
+                    return group.title.substr(0, 1)
+                })
+            }
+        },
         created() {
             this.probeType = 3
+            this.touch = {}
+        },
+        methods: {
+            onShortcutTouchStart(e) {
+                let anchorIndex = getData(e.target, 'index')
+                let firstTouch = e.touches[0]
+                this.touch.y1 = firstTouch.pageY
+                this.touch.anchorIndex = anchorIndex
+                this._scrollTo(anchorIndex)
+            },
+            onShortcutTouchMove(e) {
+                let firstTouch = e.touches[0]
+                this.touch.y2 = firstTouch.pageY
+                let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+                let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+                this._scrollTo(anchorIndex)
+            },
+            _scrollTo(index) {
+                this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+            }
         }
     }
 </script>
@@ -73,4 +121,25 @@
                     color: $color-text-l
                     font-size: $font-size-medium
 
+        .list-shortcut
+            position: absolute
+            z-index: 30
+            right: 0
+            top: 50%
+            transform: translateY(-50%)
+            width: 20px
+            padding: 20px 0
+            border-radius: 10px
+            text-align: center
+            background: $color-background-d
+            font-family: Helvetica
+
+            .item
+                padding: 3px
+                line-height: 1
+                color: $color-text-l
+                font-size: $font-size-small
+
+                &.current
+                    color: $color-theme
 </style>
