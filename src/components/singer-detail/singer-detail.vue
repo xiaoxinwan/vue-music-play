8a989f5d01655ceecc2e8d5f59d58e3eef69b0d4
xiaoxinwan <lixinxi1010@gmail.com>
2019-04-09 14:18:30 +0800

    新增singer-detail组件，加入转场动画

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <transition name="slide">
+        <div class="singer-detail"></div>
+    </transition>
 </template>
 
 <script>
@@ -8,6 +10,20 @@
     }
 </script>
 
-<style scoped lang='scss'>
+<style lang='stylus'>
+    @import "../../assets/stylus/variable.styl"
 
+    .singer-detail
+        position: fixed
+        top: 0
+        left: 0
+        bottom: 0
+        right: 0
+        z-index: 100
+        background: $color-background
+
+    .slide-enter-active, .slide-leave-active
+        transition: all .3s
+    .slide-enter, .slide-leave-to
+        transform: translate3d(100%, 0, 0)
 </style>
