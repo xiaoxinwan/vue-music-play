17b578744e0fa74c13d1fc1eb27af851ca8ed940
xiaoxinwan <lixinxi1010@gmail.com>
2019-04-13 22:08:57 +0800

    添加progress-circle组件，实现进度圆转动功能

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -64,7 +64,9 @@
                     <p class="desc" v-html="currentSong.singer"></p>
                 </div>
                 <div class="control">
-                    <i @click.stop="togglePlaying" :class="miniIcon"></i>
+                    <progress-circle :radius="radius" :percent="percent">
+                        <i @click.stop="togglePlaying" class="icon-mini" :class="miniIcon"></i>
+                    </progress-circle>
                 </div>
                 <div class="control">
                     <i class="icon-playlist"></i>
@@ -85,18 +87,21 @@
     import animations from 'create-keyframe-animation'
     import {prefixStyle} from "../../assets/js/dom"
     import ProgressBar from '../../base/progress-bar/progress-bar'
+    import ProgressCircle from '../../base/progress-circle/progress-circle'
 
     const transform = prefixStyle('transform')
 
     export default {
         name: "player",
         components: {
-            ProgressBar
+            ProgressBar,
+            ProgressCircle
         },
         data() {
             return {
                 songReady: false,
                 currentTime: null,
+                radius: 32
             }
         },
         computed: {
